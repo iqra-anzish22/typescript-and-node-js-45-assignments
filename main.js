dad3b41084d63d0a687189d6a5bc959600b7e349
Iqra <iqraanzish48@gmail.com>
2024-06-06 11:41:24 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+"use strict";
+// // Q11
+// // let friendName=["iqra", "saba","sana"]
+// // console.log(friendName[0])
+// // console.log(friendName[1])
+// // console.log(friendName[2])
+Object.defineProperty(exports, "__esModule", { value: true });
+// // Q12
+// // let friendName=["iqra", "saba","sana"]
+// // let message="how are you"
+// //  console.log("Hello",friendName[0],message)
+// //  console.log("Hello",friendName[1],message)
+// //  console.log("Hello",friendName[2],message)
+// // Q13
+// // let transpotation : string [] = ["Honda Motorcycle","Audi","Honda City"];
+// // transpotation.map ((items) => console.log(`I would like to own a ${items}`));
+// // Q14
+// // let guestArr : string [] = ["Ali","Saba","Sana"];
+// // guestArr.map ((items) =>(console.log(`Dear ${items} , Yor are invited to the dinner`)));
+// // // Q15
+// // let guestArr : string[] = ["Ali","Saba","Sana","Dua"];
+// // let canNotAttend : String = "Ali"
+// // // console.log(canNotAttend + "" "can not attend the dinner.")
+// // let newGuest : string = "alishba"
+// // guestArr [guestArr.indexOf(canNotAttend)] = newGuest
+// // // console.log(guestArr)
+// // guestArr.map((items) => console.log (`Dear ${items} , you are invited to the dinner.`))
+// // Q16
+//   let guestArr : string[] = ["Ali","Saba","Sana","Dua"];
+//   let canNotAttend : string = "Saba"
+//   let newGuest : string ="Hamza"
+//   guestArr[guestArr.indexOf(canNotAttend)] = newGuest;
+//  console.log(guestArr)
+// //  guestArr.map((items) =>
+// // console.log(`Dear ${items}, I found a bigger dinner table so I am invited more people.`));
+//  let guestbeg : string = "laiba"
+//  guestArr.unshift(guestbeg);
+//  console.log(guestArr)
+
+//  let middleGuest : string ="Fatima"
+//  let middleIndex = guestArr.length/3
+//  guestArr.splice(middleIndex,0,middleGuest)
+//  console.log(guestArr)
+
+// guestArr.push("iqra")
+// console.log(guestArr)
+
+// guestArr.map((items) =>
+// console.log(`Dear ${items}, you are invited in the more people list on dinner`));
+
+ 
